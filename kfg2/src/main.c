6145bf09473960435ed8038092696336ec7d44bd
ronaldo <ronaldo@cheesetea.com>
2023-04-10 18:06:30 +0200

    KFG2: First tests

--- kfg2/src/main.c
+++ kfg2/src/main.c
@@ -1,36 +1,29 @@
-//-----------------------------LICENSE NOTICE------------------------------------
-//  This file is part of CPCtelera: An Amstrad CPC Game Engine
-//  Copyright (C) 2018 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Lesser General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//------------------------------------------------------------------------------
-
 #include <cpctelera.h>
+#include <assets/g_palette.h>
+#include <assets/princess.h>
+
+#define SCR_PTR_U16(VMEM,X,Y) (u16)((VMEM) + 80 * ((u16)((Y) >> 3)) + 2048 * ((Y) & 7) + (X))
+
+void init() {
+   cpct_disableFirmware();
+   cpct_setVideoMode(0);
+   cpct_setPalette(g_palette, 16);
+   cpct_setBorder(HW_LIME);
+}
+
+void game() {
+   enum {
+      p_string = SCR_PTR_U16(0xC000,  0,  96),
+      p_princ  = SCR_PTR_U16(0xC000, 38, 140)
+   };
+   cpct_setDrawCharM0(1, 0);
+   cpct_drawStringM0("Welcome to CPCtelera!", (void*)p_string);
+   cpct_drawSprite(sp_princess_0, (void*)p_princ, SP_PRINCESS_0_W, SP_PRINCESS_0_H);
+
+   while (1);
+}
 
 void main(void) {
-   u8* pvmem;  // Pointer to video memory
-
-   // Make pvmem point to the byte in video memory where we want
-   // to print our string (coordinates (20, 96) in bytes, (80, 96) in pixels,
-   // as each byte is 4 pixels wide in mode 1)
-   pvmem = cpct_getScreenPtr(CPCT_VMEM_START, 20, 96);
-   // Set Mode 1 Character&String drawing functions to use colours 
-   // 1 (yellow) for foreground and 0 (blue) for background
-   cpct_setDrawCharM1(1, 0);
-   // Draw the actual string where pvmem is pointing
-   cpct_drawStringM1("Welcome to CPCtelera!", pvmem);
-
-   // Loop forever
-   while (1);
+   init();
+   game();
 }
